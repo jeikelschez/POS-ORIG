ba4a414bfa83078abbeb24e3b4c4fad89b681e38
Jeimmy <jrollinsanchez@gmail.com>
2019-10-10 21:49:48 -0400

    se coloco el title y se modifico el footer

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos",
-                      "jeikel",
-  						        "Spider",
+		$link = new PDO("mysql:host=localhost;dbname=simgedic",
+                      "root",
+  						        "",
   						        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
   						        );
--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 
-	<strong>Copyright &copy; 2019 <a href="https://abundiss.com/" target="_blank">Abundiss Services</a>.</strong>
+	<strong>Copyright &copy; 2019 <a href="https://www.innovseo.com/" target="_blank">Innovseo C.A</a>.</strong>
 
 	Todos los derechos reservados.
 

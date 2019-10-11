d5b549b1f69569ce4efca89159e647e4914f3074
Jeikel Sanchez <jeikelschez@hotmail.com>
2019-10-10 21:53:26 -0400

    favicon

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -12,6 +12,8 @@ session_start();
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+  <link rel="icon" href="vistas/img/plantilla/icono-negro.png">
+
   <!--*=============================================
     PLANTILLAS CSS
     =============================================-->

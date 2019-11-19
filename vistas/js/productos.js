569b1be2954d754b7bbdb56144a1741b8771c093
Jeimmy <jrollinsanchez@gmail.com>
2019-11-18 22:25:29 -0400

    bloqueo de codigo

--- vistas/js/productos.js
+++ vistas/js/productos.js
@@ -411,10 +411,10 @@ $(".tablaProductos tbody").on("click", ".conversorPrecio", function(){
   })
 
 })
-=============================================
+/*=============================================
 DESHABILITAR CLICK DERECHO / F12 Y CTRL+SHIFT+I
 =============================================*/
-/*$(document).ready(function(){
+$(document).ready(function(){
    
   $(document).bind("contextmenu",function(e){
     return false;
